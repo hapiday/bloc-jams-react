1ba0ba484ccb6b8cd73912812fe0b86fb196d82a
Sara <sarakang05@gmail.com>
2018-09-09 19:21:14 -0700

    album

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,7 +9,7 @@ const Landing = () => (
      <h2 className="point-title">Choose your music</h2>
      <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
     </div>
-    
+
     <div className="point">
      <h2 className="point-title">Unlimited, streaming, ad-free</h2>
      <p className="point-description">No arbitrary limits. No distractions.</p>
